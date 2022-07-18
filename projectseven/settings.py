23709b343668a77c93b2c21f350bd7bcd28e1364
Nikkjack21 <jackson97patrick@gmail.com>
2022-07-18 22:02:19 +0530

    sqllite

--- projectseven/settings.py
+++ projectseven/settings.py
@@ -96,34 +96,34 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'zhea_database',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'jackson615',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
-    }
-
-}
-
-
-
 # DATABASES = {
+
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'zhea_database',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'jackson615',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
 #     }
+
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
